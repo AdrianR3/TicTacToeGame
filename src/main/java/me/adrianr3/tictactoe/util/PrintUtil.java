1df91a18b9f4f508c715d1bf127c0b13972d9e2b
AdrianR3 <93000948+AdrianR3@users.noreply.github.com>
2024-07-10 02:16:46 -0700

    Upgraded board printing

--- src/main/java/me/adrianr3/tictactoe/util/PrintUtil.java
+++ src/main/java/me/adrianr3/tictactoe/util/PrintUtil.java
@@ -8,7 +8,10 @@ import java.util.Arrays;
 public class PrintUtil {
     public static void printBoard(GameBoard board, PrintStream sout) {
         StringBuilder sb = new StringBuilder();
-        sb.append("    ");
+        sb.append("   ");
+        if (board.getBoardArray().length > 10) sb.append(' ');
+
+//        Print header
         for (int i = 0; i < board.getBoardArray()[0].length; i++) {
             sb.append(i);
             if (i < board.getBoardArray()[0].length - 1) {
@@ -16,12 +19,17 @@ public class PrintUtil {
                 if (i < 10) sb.append(' ');
             }
         }
+
+//        Print array and left header
         sb.append(" \n");
         int i = 0;
         for (int[] row : board.getBoardArray()) {
+//            Print header
             sb.append(i);
             sb.append(' ');
-            if (i++ < 10) sb.append(' ');
+            if (i++ < 10 && board.getBoardArray().length > 10) sb.append(' ');
+
+//            Print row
             sb.append(Arrays.toString(row)).append("\n");
         }
 
